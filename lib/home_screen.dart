ecff1e608af8b4f68309fd40da05fb2604ba45f0
Amitesh Pandey <amiteshpandey1801@gmail.com>
2022-09-28 01:17:23 +0530

    body added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -16,8 +17,37 @@ class HomeScreen extends StatelessWidget {
         ),
         actions: [
           IconButton(
-            onPressed: () => {}, 
-            icon : SvgPicture.asset("assets/icons/Cart.svg"))],
+            onPressed: () => {},
+            icon: SvgPicture.asset("assets/icons/Cart.svg"),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Text(
+            "Explore",
+            style: Theme.of(context)
+                .textTheme
+                .headline4!
+                .copyWith(fontWeight: FontWeight.w500, color: Colors.black),
+          ),
+          Text(
+            'the best outfits for you',
+            style: TextStyle(fontSize: 18),
+          )
+          GridView.builder(
+            padding: const EdgeInsets.all(10.0),
+            itemCount: ,
+            itemBuilder: (ctx , i) =>Container(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, 
+              childAspectRatio: 3/2, 
+              crossAxisSpacing: 10, 
+              mainAxisSpacing: 10,), )
+        ]),
       ),
     );
   }
